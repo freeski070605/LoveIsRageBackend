8ca21d7b2d59f9e292ce10772732425ed8ab7b51
Freeski070605 <freeski0706@outlook.com>
2026-01-28 00:45:01 -0500

    add CORS middleware and configure options for cross-origin requests

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import connectDB from './db.js';
 import productRoutes from './routes/productRoutes.js';
 import authRoutes from './routes/authRoutes.js';
@@ -13,6 +14,14 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: ['https://loveisrage.vercel.app', 'http://localhost:3000', 'http://localhost:5000'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
