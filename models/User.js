0fbcc8bfb5ed68ab48a062738f20dbbb65452d79
Freeski070605 <freeski0706@outlook.com>
2026-01-28 01:46:49 -0500

    fix: remove unnecessary next() calls in user model and auth routes

--- models/User.js
+++ models/User.js
@@ -22,14 +22,13 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Hash password before saving the user
-UserSchema.pre("save", async function (next) {
+UserSchema.pre("save", async function () {
   if (!this.isModified("password")) {
-    next();
+    return;
   }
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 const User = mongoose.model("User", UserSchema);
