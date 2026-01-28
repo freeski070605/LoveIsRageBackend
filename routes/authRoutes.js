4076f96ad58e68132c4640a0441d9480dc974a10
Freeski070605 <freeski0706@outlook.com>
2026-01-28 01:13:26 -0500

    refactor: rename 'name' to 'Username' in User model and auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.post(
   '/register',
   asyncHandler(async (req, res, next) => {
-      const { name, email, password } = req.body;
+      const { Username, email, password } = req.body;
   
       const userExists = await User.findOne({ email });
   
@@ -22,7 +22,7 @@ router.post(
   
       try {
         const user = await User.create({
-          name,
+          Username,
           email,
           password,
         });
@@ -30,7 +30,7 @@ router.post(
         if (user) {
           res.status(201).json({
             _id: user._id,
-            name: user.name,
+            Username: user.Username,
             email: user.email,
             isAdmin: user.isAdmin,
             token: generateToken(user._id),
@@ -65,7 +65,7 @@ router.post(
     if (user && (await user.matchPassword(password))) {
       res.json({
         _id: user._id,
-        name: user.name,
+        Username: user.Username,
         email: user.email,
         isAdmin: user.isAdmin,
         token: generateToken(user._id),
